0ebee7489068b6dd15cb02b19ff7b1edde31ad18
natrux <tmpdf1f71a7@tefrod.de>
2024-02-26 16:36:49 +0100

    keyboard controls

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int /*argc*/, char **/*argv*/){
 	double zoom = 0.005;
 	double offset_x = screen_width * zoom * 2 / 3;
 	double offset_y = screen_height * zoom / 2;
-	const unsigned long iter_limit = 100;
+	unsigned long iter_limit = 100;
 	const bool use_opencl = false;
 
 	if(SDL_Init(SDL_INIT_VIDEO) < 0){
@@ -48,18 +48,83 @@ int main(int /*argc*/, char **/*argv*/){
 		mandelbrot_renderer = std::make_unique<MandelbrotRenderer>(screen_width, screen_height, texture);
 	}
 	mandelbrot_renderer->set_iter_limit(iter_limit);
-	mandelbrot_renderer->draw(zoom, offset_x, offset_y);
 
 
-	if(SDL_RenderClear(renderer) < 0){
-		throw std::runtime_error("SDL_RenderClear() failed with: " + std::string(SDL_GetError()));
+
+	bool do_run = true;
+	bool do_draw = true;
+	while(do_run){
+		if(do_draw){
+		mandelbrot_renderer->draw(zoom, offset_x, offset_y);
+			if(SDL_RenderClear(renderer) < 0){
+				throw std::runtime_error("SDL_RenderClear() failed with: " + std::string(SDL_GetError()));
+			}
+			if(SDL_RenderCopy(renderer, texture, NULL, &draw_area) < 0){
+				throw std::runtime_error("SDL_RenderCopy() failed with: " + std::string(SDL_GetError()));
+			}
+			SDL_RenderPresent(renderer);
+			do_draw = false;
+		}
+
+		SDL_Event event;
+		if(SDL_WaitEvent(&event) == 0){
+			do_run = false;
+		}
+
+		if(event.type == SDL_QUIT){
+			do_run = false;
+		}else if(event.type == SDL_KEYDOWN && event.key.type == SDL_KEYDOWN){
+			switch(event.key.keysym.sym){
+			case SDLK_ESCAPE:
+				do_run = false;
+				break;
+			case SDLK_LEFT:
+				offset_x += 50*zoom;
+				do_draw = true;
+				break;
+			case SDLK_RIGHT:
+				offset_x -= 50*zoom;
+				do_draw = true;
+				break;
+			case SDLK_UP:
+				offset_y += 50*zoom;
+				do_draw = true;
+				break;
+			case SDLK_DOWN:
+				offset_y -= 50*zoom;
+				do_draw = true;
+				break;
+			case SDLK_PLUS:
+				zoom /= 2;
+				offset_x -= (screen_width / 2) * zoom;
+				offset_y -= (screen_height / 2) * zoom;
+				do_draw = true;
+				break;
+			case SDLK_MINUS:
+				offset_x += (screen_width / 2) * zoom;
+				offset_y += (screen_height / 2) * zoom;
+				do_draw = true;
+				zoom *= 2;
+				break;
+			case SDLK_SPACE:
+				mandelbrot_renderer->renew_colors();
+				do_draw = true;
+				break;
+			case SDLK_PAGEUP:
+				iter_limit += 10;
+				mandelbrot_renderer->set_iter_limit(iter_limit);
+				do_draw = true;
+				break;
+			case SDLK_PAGEDOWN:
+				iter_limit -= std::min(static_cast<unsigned long>(10), iter_limit-1);
+				mandelbrot_renderer->set_iter_limit(iter_limit);
+				do_draw = true;
+				break;
+			default:
+				break;
+			}
+		}
 	}
-	if(SDL_RenderCopy(renderer, texture, NULL, &draw_area) < 0){
-		throw std::runtime_error("SDL_RenderCopy() failed with: " + std::string(SDL_GetError()));
-	}
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(5000);
 
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
